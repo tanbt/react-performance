82e42f1d380d89b9a1609c987dac34947838029e
tan.bui@virta.global <tan.bui@virta.global>
2024-04-20 17:11:30 +0300

    Separate component to memorize based on props

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -96,8 +96,14 @@ Grid = React.memo(Grid)
 function Cell({row, column}) {
   const state = useAppState()
   const cell = state.grid[row][column]
+  return <CellImpl cell={cell} row={row} column={column} />
+}
+Cell = React.memo(Cell)
+
+function CellImpl({cell, row, column}) {
   const dispatch = useAppDispatch()
   const handleClick = () => dispatch({type: 'UPDATE_GRID_CELL', row, column})
+
   return (
     <button
       className="cell"
@@ -111,7 +117,7 @@ function Cell({row, column}) {
     </button>
   )
 }
-Cell = React.memo(Cell)
+CellImpl = React.memo(CellImpl)
 
 function DogNameInput() {
   const [dogName, setDogName] = useDogState()
